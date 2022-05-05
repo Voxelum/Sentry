38b7cfc26ea49ce1ba8ea0b94d151779e5c7ab4c
dozono <chrislee.hao@gmail.com>
2022-05-05 19:46:41 +0800

    1.18

--- src/main/java/voxelum/sentry/Sentry.java
+++ src/main/java/voxelum/sentry/Sentry.java
@@ -1,27 +1,24 @@
 package voxelum.sentry;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.SoundType;
-import net.minecraft.block.material.Material;
-import net.minecraft.client.gui.ScreenManager;
-import net.minecraft.inventory.container.ContainerType;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.potion.Effect;
-import net.minecraft.tileentity.TileEntityType;
+import com.mojang.blaze3d.shaders.Effect;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.DistExecutor;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import voxelum.sentry.block.SentryBaseBlock;
 import voxelum.sentry.block.SentryShooterBlock;
 import voxelum.sentry.block.SentrySuppBlock;
@@ -34,11 +31,10 @@ import voxelum.sentry.tileentity.SentryShooterTileEntity;
 public class Sentry {
     public static final String MODID = "sentry";
 
-    private static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MODID);
-    private static final DeferredRegister<Effect> EFFECTS = new DeferredRegister<>(ForgeRegistries.POTIONS, MODID);
-    private static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, MODID);
-    private static final DeferredRegister<TileEntityType<?>> TILE = new DeferredRegister<>(ForgeRegistries.TILE_ENTITIES, MODID);
-    private static final DeferredRegister<ContainerType<?>> CONTAINER = new DeferredRegister<>(ForgeRegistries.CONTAINERS, MODID);
+    private static final DeferredRegister<Item> ITEMS =DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    private static final DeferredRegister<Block> BLOCKS =DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+    private static final DeferredRegister<BlockEntityType<?>> TILE =DeferredRegister.create(ForgeRegistries.BLOCK_ENTITIES, MODID);
+    private static final DeferredRegister<MenuType<?>> CONTAINER =DeferredRegister.create(ForgeRegistries.CONTAINERS, MODID);
 
     public static final String SENTRY_SUPP_BLOCK_NAME = "sentry_supp_block";
     public static final String SENTRY_BASE_BLOCK_NAME = "sentry_base_block";
@@ -46,9 +42,11 @@ public class Sentry {
 
     public static final RegistryObject<Block> SENTRY_BASE_BLOCK = BLOCKS.register(SENTRY_BASE_BLOCK_NAME,
             () -> new SentryBaseBlock(Block.Properties
-                    .create(Material.WOOD)
-                    .hardnessAndResistance(2F, 6F)
-                    .sound(SoundType.WOOD)));
+                    .of(Material.WOOD)
+                    .explosionResistance(6F)
+                    .destroyTime(2f)
+                    .sound(SoundType.WOOD)
+                    .);
     public static final RegistryObject<Item> SENTRY_BASE_BLOCK_ITEM = ITEMS.register(SENTRY_BASE_BLOCK_NAME,
             () -> new BlockItem(SENTRY_BASE_BLOCK.get(), new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)));
     public static final RegistryObject<TileEntityType<SentryBaseTileEntity>> SENTRY_BASE_TILE_ENTITY = TILE.register("sentry_base_tile_entity",
--- src/main/java/voxelum/sentry/block/SentryBaseBlock.java
+++ src/main/java/voxelum/sentry/block/SentryBaseBlock.java
@@ -1,22 +1,12 @@
 package voxelum.sentry.block;
 
-import net.minecraft.block.*;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.InventoryHelper;
-import net.minecraft.inventory.container.INamedContainerProvider;
-import net.minecraft.item.BlockItemUseContext;
-import net.minecraft.item.ItemStack;
-import net.minecraft.state.DirectionProperty;
-import net.minecraft.state.StateContainer;
-import net.minecraft.tileentity.AbstractFurnaceTileEntity;
-import net.minecraft.tileentity.TileEntity;
-import net.minecraft.util.*;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorld;
-import net.minecraft.world.World;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.HorizontalDirectionalBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraftforge.items.IItemHandler;
 import voxelum.sentry.Sentry;
 import voxelum.sentry.container.SentryBaseContainer;
@@ -24,12 +14,12 @@ import voxelum.sentry.tileentity.SentryBaseTileEntity;
 
 import javax.annotation.Nullable;
 
-public class SentryBaseBlock extends ContainerBlock {
-    public static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
+public class SentryBaseBlock extends BaseEntityBlock {
+    public static final DirectionProperty FACING = HorizontalDirectionalBlock.FACING;
 
     public SentryBaseBlock(Properties properties) {
         super(properties);
-        this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
+        this.defaultBlockState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
     }
 
     @Override
@@ -102,5 +92,11 @@ public class SentryBaseBlock extends ContainerBlock {
     @Override
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.CUTOUT;
+    }
+
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
+        return null;
     }
 }
